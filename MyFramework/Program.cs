f0f0d514ff9e12e3fe43da3fdb68d56ecec09abc
AlexBlok1083 <alexblok50@gmail.com>
2022-03-01 09:44:49 +0100

    Initial commit

---
+++ MyFramework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFramework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello");
+			AsteroidGame game = new AsteroidGame();
+			game.Play();
+		}
+	}
+}
